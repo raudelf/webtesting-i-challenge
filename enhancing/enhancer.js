9af37be827814f795bbe24a61508eae177eb6f16
raudelf <rawdell@outlook.com>
2020-04-29 19:24:59 -0500

    reformatted what the succeed function returns

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,38 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  const {name, enhancement} = item;
+
+  if (enhancement == 20) {
+    return (
+      `${name} is already at max enhancement!`, {...item}
+      )
+  } else {
+    return { ...item, enhancement: enhancement + 1 };
+  }
 }
 
 function fail(item) {
-  return { ...item };
-}
+  const {enhancement, durability} = item;
+
+  if (enhancement < 15) {
+    return {...item, durability: durability - 5}
+  } else if (enhancement > 14) {
+    return {...item, durability: durability - 10}
+  } else if (enhancement > 16) {
+    return {...item, enhancement: enhancement - 1}
+  };
+  
+};
 
 function repair(item) {
-  return { ...item };
+  const {enhancement, durability} = item;
+
+  if (enhancement > 20 || enhancement < 0 || durability > 100 || durability < 0) {
+    return 'Item enhancement or durability is not within range!'
+  } else {
+    return { ...item, durability: 100 };
+  }
 }
 
 function get(item) {
