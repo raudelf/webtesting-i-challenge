c1d2608376d7bd9bbc00560e491e113e7443c420
raudelf <rawdell@outlook.com>
2020-04-29 19:26:05 -0500

    finished the tests for the repair function

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,27 @@
-const enhancer = require('./enhancer.js');
+const {succeed, fail, repair, get} = require('./enhancer.js');
 // test away!
+
+describe('repair', () => {
+    it('Should bring durability to 100', () => {
+        const item = {
+            enhancement: 10,
+            durability: 20
+        };
+        
+        const expectedResult = {
+            enhancement: 10,
+            durability: 100
+        };
+
+        expect(repair(item)).toEqual(expectedResult);
+    });
+
+    it('Should return a string error', () => {
+        const item = {
+            enhancement: 25,
+            durability:10
+        };
+
+        expect(repair(item)).toBe("Item enhancement or durability is not within range!");
+    });
+});
